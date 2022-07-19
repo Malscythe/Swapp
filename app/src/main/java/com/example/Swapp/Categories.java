34493cd0a10f359966bdc8ddd2720527d0e41592
Terence <terence.stfu.ts@gmail.com>
2022-07-19 22:19:23 +0800

    v8

---
+++ app/src/main/java/com/example/Swapp/Categories.java
@@ -0,0 +1,15 @@
+package com.example.Swapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import Swapp.R;
+
+public class Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
